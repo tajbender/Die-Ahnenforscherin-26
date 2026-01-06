fefb274b94f8cfae041f7e0706944727b43dfe2d
Thorsten Jung <tajbender@users.noreply.github.com>
2026-01-06 20:22:29 +0100

    wip: add Navigation handler to `NavigationView`

--- Die Ahnenforscherin 26/MainWindow.xaml.cs
+++ Die Ahnenforscherin 26/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
+using Die_Ahnenforscherin_26.Pages;
 using Microsoft.UI.Xaml;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Xaml.Controls;
+using System.Diagnostics;
+using Windows.Devices.Enumeration;
 
 namespace Die_Ahnenforscherin_26
 {
@@ -14,6 +15,26 @@ namespace Die_Ahnenforscherin_26
         {
             InitializeComponent();
             this.ExtendsContentIntoTitleBar = true;
+
+//            NavigateTo();  // https://stackoverflow.com/questions/74329160/c-sharp-winui-3-frame-navigation-stack
+
+//            var titleBar = this.ContentFrame.XamlRoot.Content as FrameworkElement;
+//            this.SetTitleBar(titleBar);
+
+//            this.ContentFrame.Navigate(typeof(WorkbenchPage), null);
+
+
+            /*(sender as Frame).Navigate(typeof(MainPage), (sender as Frame).XamlRoot);*/
+        }
+
+        private void NavigataionView_SelectionChanged(NavigationView _,
+            NavigationViewSelectionChangedEventArgs args)
+        {
+            Debug.WriteLine("NavigationView_SelectionChanged");
+
+            // Play the default animation ContentFrame
+            //ContentFrame.Navigate(typeof(WorkbenchPage), null);
+            this.ContentFrame.Navigate(typeof(WorkbenchPage), null);
         }
     }
 }
